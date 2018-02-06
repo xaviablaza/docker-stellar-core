c9224cd55c9cd0817b749993db4a1f10c7355577
Ramon Tayag <ramon@tayag.net>
2018-02-06 10:30:04 +0800

    Do not setup gsutil if GS_SERVICE_ACCOUNT_KEY is not set

--- entry.sh
+++ entry.sh
@@ -20,6 +20,7 @@ function stellar_core_init_db() {
 }
 
 function setup_gsutil() {
+  [ -n $GS_SERVICE_ACCOUNT_KEY ] || return 0
   echo "Setting up gsutil..."
   echo $GS_SERVICE_ACCOUNT_KEY > /tmp/gcloud-key.json
   gcloud auth activate-service-account --key-file /tmp/gcloud-key.json
